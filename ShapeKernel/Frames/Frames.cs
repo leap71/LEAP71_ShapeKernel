27480294614b708c0dbd4a9be02c85d42f30f0d9
Josefine Lissner <44703282+josefinelissner@users.noreply.github.com>
2025-07-12 20:34:29 +0400

    Update Frames.cs

--- ShapeKernel/Frames/Frames.cs
+++ ShapeKernel/Frames/Frames.cs
@@ -200,9 +200,9 @@ namespace Leap71
                 float fMaxDotProduct    = MathF.Abs(Vector3.Dot(vecInitLocalX, vecTargetX));
                 Vector3 vecFinalLocalX  = vecInitLocalX;
 
-                for (float dAngle = 0; dAngle < 180; dAngle += 0.1f)
+                for (float dAngle = 0; dAngle < 180f; dAngle += 0.1f)
                 {
-                    float fPhi              = (2 * MathF.PI) / 360 * dAngle;
+                    float fPhi              = (2 * MathF.PI) / 360f * dAngle;
                     Vector3 vecNewLocalX    = MathF.Cos(fPhi) * vecInitLocalX + MathF.Sin(fPhi) * vecInitLocalY;
                     float fDotProduct       = MathF.Abs(Vector3.Dot(vecNewLocalX, vecTargetX));
                     if (fDotProduct > fMaxDotProduct)
