37f124a43ab0ed23f17ecbb914fd3898225791f8
Josefine Lissner <44703282+josefinelissner@users.noreply.github.com>
2025-11-13 14:51:59 +0400

    Create RotationAnimator.cs

---
+++ ShapeKernel/Visualizations/RotationAnimator.cs
@@ -0,0 +1,68 @@
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// The LEAP 71 ShapeKernel is an open source geometry engine
+// specifically for use in Computational Engineering Models (CEM).
+//
+// For more information, please visit https://leap71.com/shapekernel
+// 
+// This project is developed and maintained by LEAP 71 - © 2024 by LEAP 71
+// https://leap71.com
+//
+// Computational Engineering will profoundly change our physical world in the
+// years ahead. Thank you for being part of the journey.
+//
+// We have developed this library to be used widely, for both commercial and
+// non-commercial projects alike. Therefore, have released it under a permissive
+// open-source license.
+// 
+// The LEAP 71 ShapeKernel is based on the PicoGK compact computational geometry 
+// framework. See https://picogk.org for more information.
+//
+// LEAP 71 licenses this file to you under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with the
+// License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, THE SOFTWARE IS
+// PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.   
+//
+
+
+using PicoGK;
+
+
+namespace Leap71
+{
+    namespace ShapeKernel
+    {
+        public class RotationAnimator
+        {
+            /// <summary>
+            /// When this class is instantiated, it starts to rotate the view port against
+            /// the objects currently on display. This can be used for screen-capturing videos.
+            /// The animation will go on indefinitely.
+            /// </summary>
+            public RotationAnimator()
+            {
+                while (true)
+                {
+                    Uf.Wait(0.01f);
+                    Do();
+                }
+            }
+
+            public void Do()
+            {
+                float fElevation    = Library.oViewer().m_fElevation;
+                float fCurrentOrbit = Library.oViewer().m_fOrbit;
+                float fNextOrbit    = fCurrentOrbit + 0.5f;
+                Library.oViewer().SetViewAngles(fNextOrbit, fElevation);
+            }
+        }
+    }
+}
